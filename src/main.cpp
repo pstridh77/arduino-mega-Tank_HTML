5e0af5bc53f3c4c76a1983dbd1bebc151742ac42
Patrik Stridh <pstridh77@gmail.com>
2026-09-16 21:05:11 +0200

    LED Test Working

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
 
 void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+
   Serial.begin(115200);
+  Serial.println("READY");
 }
 
 void loop() {
-  Serial.println("Hej!");
-  delay(1000);
+  if (Serial.available() == 0) {
+    return;
+  }
+
+  const char command = Serial.read();
+
+  if (command == '1') {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("LED:ON");
+  } else if (command == '0') {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("LED:OFF");
+  }
 }
